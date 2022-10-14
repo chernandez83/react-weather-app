9a3ac50b95003ddbd11a650f7075c18a457e1efc
chernandez83 <104525460+chernandez83@users.noreply.github.com>
2022-10-14 14:20:52 -0500

    Complete project

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '75c9f1784dmshb677fb6c30d5fb6p1067c6jsndb3ff8e2737e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "6d4d146ef19ca5f9581096e6b6eed38a";
